0342a0fa58dfd7a7200dc06035f9c407edc395f3
iqbaldevdf <mohammed.iqbal@datafortune.com>
2026-03-15 14:37:35 +0530

    Delete voice-room directory

--- voice-room/frontend/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-const socket = io(import.meta.env.VITE_WS_URL || 'http://localhost:3002', {
-  autoConnect: false,
-  transports: ['websocket'],
-});
-
-export default socket;
